d019c0885479a3aebbb5991e53bf19670506a18f
AirenMarie <astringer92@gmail.com>
2024-08-09 21:27:07 -0400

    store elements, declare let variables, create event listener for viewing game rules

---
+++ dice-game.js
@@ -0,0 +1,26 @@
+const listOfAllDice = document.querySelector(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRound = document.getElementById("current-round");
+const currentRoundRolls = document.getElementById("current-round-rolls");
+const totalScore = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rules-btn");
+const rulesContainer = document.querySelector(".rules-container");
+
+let isModalShowing = false;
+
+let diceValuesArr = [];
+
+let rolls = 0;
+let score = 0;
+let total = 0;
+let round = 1;
+
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
+  rulesBtn.innerText = isModalShowing ? "Hide rules" : "Show rules";
+  rulesContainer.style.display = isModalShowing ? "block" : "none";
+});
